e77b0f141fbf764523751aaf001b185554ea17bd
larissa08senai <Larissa.siqueira62608@gmail.com>
2025-09-10 09:20:55 -0300

    Update ContaTest.java

--- aula04/ContaTest.java
+++ aula04/ContaTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ContaTest {
 	public static void main(String[] args) {
 		Conta pessoa1 = new Conta();
-		
+		int dado ;
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.println("ID: ");
@@ -19,10 +19,26 @@ public class ContaTest {
 		
 		
 		pessoa1.exibirConta();
+
+		 System.out.println("sacar= 1  depositar = 2");
+		dado = ler.nextInt();
 		
-		
-		
+		if(dado == 1) {
+			 System.out.println("Valor do saque");
+			 pessoa1.setSaque(ler.nextDouble());
+			 
+			 pessoa1.sacar();
+		}else {
+			 System.out.println("valor a depositar");
+			 pessoa1.setDeposito(ler.nextDouble());
+			 pessoa1.depositar();
+			 System.out.println(pessoa1.getSaldo());
+		}
+		ler.close();
 	}
+		
+		
+}
 
 	
 }
